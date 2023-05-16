3ba26bc5325c05007923d6cc1bf7085bbd884a30
guilherme.monteiro.espim@gmail.com <rm97937@fiap.com.br>
2023-05-16 16:32:09 -0300

    cp2 done

--- js/cp2.js
+++ js/cp2.js
@@ -1,21 +1,41 @@
+// pegando a tag form e o component dig com id alert no HMTL
 const form = document.querySelector("#form");
+const alertHtml = document.querySelector("#alert");
 
+
+// settando o darkmode para false
+let mode = false;
+
+
+// Armazenando os principais botões e inputs em um array para o código não ficar repetitivo
 const inputs = [
     document.querySelector("#name"),
     document.querySelector("#surname"),
     document.querySelector("#email"),
     document.querySelector("#password"),
-    document.querySelector("#passwordConfirm")
+    document.querySelector("#passwordConfirm"),
+    document.querySelector("#mode-btn")
 ];
 
-const alertHtml = document.querySelector("#alert");
 
+// Armazenando os elementos que sofreram alteração no darkmode ou lightmode
+const elements = [
+    document.querySelector("#form-div"),
+    ...document.querySelectorAll("input"),
+    document.querySelector("h2")
+]
+
+
+// adicionando o event de submit no form que executa a função de submut
 form.addEventListener("submit", (event) => submit(event));
 
+
+// funcção de init para adicionar os devidos eventos para cada elemento dentro do array inputs
 function init() {
+    light();
     for (let item of inputs) {
         item.style.outline = "none";
-        if (item.id != "email" && item.id != "password" && item.id != "passwordConfirm") {
+        if (item.id === "name" || item.id === "surname") {
             item.addEventListener("keyup", () => {
                 const cond = required(item);
                 error(cond, item);
@@ -40,23 +60,51 @@ function init() {
                 })
             }
         }
+        if (item.id === "mode-btn") {
+            item.addEventListener("click", () => changeMode(item))
+        }
     }
-
 }
 
+
+// função para mudar o darkmode ou lightmode
+function changeMode(item) {
+    const icon = item.querySelector("img");
+    const text = item.querySelector("p");
+    const body = document.querySelector("body");
+    console.log(icon, text)
+    if (mode === false) {
+        body.style.backgroundColor = "#001025";
+        icon.src = "./img/moon.png";
+        text.innerText = "Escuro";
+        mode = true;
+        dark();
+    } else {
+        body.style.background = "#f0f0f0";
+        icon.src = "./img/sun.png";
+        text.innerText = "Claro";
+        mode = false
+        light();
+    }
+}
+
+
+// função para comparar as senhas
 function comparePass(confirm) {
     const pass = document.querySelector("#password");
     if (pass.value === confirm.value && confirm.value.length > 0) {
         return true;
-    } 
+    }
     return false;
 }
 
+
+// função de submit do formulário
 function submit(event) {
     event.preventDefault();
     let validation = [];
     for (let item of inputs) {
-        if (item.id != "email" && item.id != "password" && item.id != "passwordConfirm") {
+        if (item.id === "name" || item.id === "surname") {
             const cond = required(item);
             error(cond, item);
             validation.push(cond);
@@ -86,6 +134,8 @@ function submit(event) {
     }
 }
 
+
+// função que faz com que o campo fique com a borda vermelha
 function error(cond, item) {
     if (!cond) {
         item.style.border = "1px solid red";
@@ -94,6 +144,8 @@ function error(cond, item) {
     }
 }
 
+
+// função que torna o campo obridatório no html
 function required(value) {
     if (value.value.length < 5) {
         return false;
@@ -101,6 +153,8 @@ function required(value) {
     return true;
 }
 
+
+// função para validar o campo de email
 function validateEmail(email) {
     // validando o @
     if (!email.value.includes("@")) {
@@ -112,6 +166,8 @@ function validateEmail(email) {
     return true;
 }
 
+
+// função para validar a senha
 function validadePass(pass) {
     if (pass.value.length < 6) {
         return false;
@@ -122,10 +178,46 @@ function validadePass(pass) {
     return true;
 }
 
-// show alert animation
+
+// função de fazer o alerta aparecer na tela
 function showAlert() {
     alertHtml.style.transform = "scale(1)";
-    setTimeout(() => alertHtml.style.transform = "scale(0)", 3000);  
+    setTimeout(() => alertHtml.style.transform = "scale(0)", 3000);
 }
 
+
+function light() {
+    elements.forEach(element => {
+        if (element.nodeName === "INPUT") {
+            if (element.classList.contains("dark")) element.classList.remove("dark");
+            element.classList.add("light");
+        }
+        if (element.nodeName === "H2") {
+            element.style.color = "#000811";
+        }
+        if (element.nodeName === "DIV") {
+            if (element.classList.contains("dark-card")) element.classList.remove("dark-card");
+            element.classList.add("light-card");
+        }
+    })
+}
+
+function dark() {
+    elements.forEach(element => {
+        if (element.nodeName === "INPUT") {
+            if (element.classList.contains("light")) element.classList.remove("light");
+            element.classList.add("dark");
+        }
+        if (element.nodeName === "H2") {
+            element.style.color = "#D9D9D9";
+        }
+        if (element.nodeName === "DIV") {
+            if (element.classList.contains("light-card")) element.classList.remove("light-card");
+            element.classList.add("dark-card");
+        }
+    })
+}
+
+
+// chamando a função de init 
 init();
